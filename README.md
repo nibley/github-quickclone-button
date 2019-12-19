4a9d661bd50b64e3bb3f5ed899271a165190444a
Alex Nibley <alexnibley@gmail.com>
2019-12-18 20:21:14 -0500

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # one-click-github-clone
+Userscript that adds a button to the left
+of Github's green clone button which copies
+the SSH clone URL to your clipboard when
+you click it. Without showing a menu first.
--- script.user.js
+++ script.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         mygithubclone
+// @name         one-click-github-SSH-clone 
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
